6c4ba3bff5144fcd495bb86d56f89d84a61edee3
mtrivediLU <mtrivedi@laurentian.ca>
2026-01-11 02:02:47 -0500

    updated yml and config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  site: "https://mtrivedilu.github.io",
+  base: "/Sudbury_Temple_Website",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
